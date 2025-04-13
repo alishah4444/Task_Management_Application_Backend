5d243c168150714b11160066c6afcb10b1ec8d31
Alishah <alishahmaknojia.97@gmail.com>
2025-04-13 00:19:33 -0700

    update package.json file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', authRoutes);
 
